d88ec137239f08a802723bf1041cdb86fd223376
Axel <a.c.gonzalesllerena@gmail.com>
2017-10-12 03:40:18 -0500

    paginas web

---
+++ web/js/script.js
@@ -0,0 +1,32 @@
+//function newSite() {
+//  var site = $('#codigo').val();
+//  var iframe = document.getElementById('iframe');
+//  iframe.src = "https://www.carneuniversitario.com.pe/Search.aspx?CodAlumno=027"+site;
+//  iframe.src = iframe.src;
+//}
+
+function newSite() {
+  var iframe = document.getElementById('iframe');
+  iframe.src = "index.html";
+  iframe.src = iframe.src;
+}
+function newSite2() {
+  var iframe = document.getElementById('iframe');
+  iframe.src = "DatosGenerales.html";
+  iframe.src = iframe.src;
+}
+function newSite3() {
+  var iframe = document.getElementById('iframe');
+  iframe.src = "Pabellones.html";
+  iframe.src = iframe.src;
+}
+function newSite4() {
+  var iframe = document.getElementById('iframe');
+  iframe.src = site;
+  iframe.src = iframe.src;
+}
+function newSite5() {
+  var iframe = document.getElementById('iframe');
+  iframe.src = site;
+  iframe.src = iframe.src;
+}
